abb914b633790742dcbb64bc9347e7fb6903baf9
beatface <emma.beaton@gmail.com>
2016-03-27 16:34:55 -0500

    switched default search from puppies to #puppies

--- models/twitter_model.js
+++ models/twitter_model.js
@@ -8,13 +8,12 @@ const T = new Twit({
     consumer_secret:      twitKeys.consumer_secret,
     access_token:         twitKeys.access_token,
     access_token_secret:  twitKeys.access_token_secret,
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    timeout_ms:           60*1000
 });
 
 module.exports.index = (req, res) => {
-    T.get('search/tweets', { q: 'puppies', count: 30 }, function(err, data, response) {
+    T.get('search/tweets', { q: '#puppies', count: 30 }, function(err, data, response) {
         if (err) throw err;
-        console.log(data);
         res.send(data);
     });
 };
